0a60017a2d19f74e5224b049966229d1a5e171ad
Ascencio08 <ascesar2003@gmail.com>
2024-09-11 18:33:45 -0500

    Add files via upload

---
+++ CREATE TABLE_César Ascencio.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users1(
+	user_id int AUTO_INCREMENT PRIMARY KEY,
+	name varchar(45),
+	age int
+	email varchar(50),
+	created date
+);
---
+++ INSERT INTO_César Ascencio.sql
@@ -0,0 +1,3 @@
+INSERT INTO users1 (name, surname) VALUES ('Maria', 'López');
+INSERT INTO users1 (name, surname) VALUES ('Paco', 'Pérez');
+INSERT INTO users1 (user_id, name, surname) VALUES (11, 'El', 'Merma');
